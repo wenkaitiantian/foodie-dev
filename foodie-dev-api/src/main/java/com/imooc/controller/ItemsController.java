13fe97f52093cfc0a41e0b61ad6bdca261aef44e
wenkai <407094141@qq.com>
2020-10-29 00:13:24 +0800

    foodie-dev 商品评价-实现分页

--- foodie-dev-api/src/main/java/com/imooc/controller/ItemsController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/ItemsController.java
@@ -4,27 +4,27 @@ import com.imooc.pojo.Items;
 import com.imooc.pojo.ItemsImg;
 import com.imooc.pojo.ItemsParam;
 import com.imooc.pojo.ItemsSpec;
+import com.imooc.pojo.vo.CommentLevelCountsVO;
 import com.imooc.pojo.vo.ItemInfoVO;
 import com.imooc.service.ItemService;
 import com.imooc.utils.IMOOCJSONResult;
+import com.imooc.utils.PagedGridResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Api(value = "商品接口", tags = {"商品信息展示的相关接口"})
 @RestController
 @RequestMapping("items")
-public class ItemsController {
+public class ItemsController extends BaseController {
     @Autowired
     private ItemService itemService;
+
 
     @ApiOperation(value = "查询商品详情", notes = "查询商品详情", httpMethod = "GET")
     @GetMapping("/info/{itemId}")
@@ -47,6 +47,45 @@ public class ItemsController {
         itemInfoVO.setItemParams(itemsParam);
 
         return IMOOCJSONResult.ok(itemInfoVO);
+    }
+
+
+    @ApiOperation(value = "查询商品评价等级", notes = "查询商品评价等级", httpMethod = "GET")
+    @GetMapping("/commentLevel")
+    public IMOOCJSONResult commentLevel(
+            @ApiParam(name = "itemId", value = "商品id", required = true)
+            @RequestParam String itemId) {
+        //判断itemsId是否为空，为空返回错误
+        if (StringUtils.isBlank(itemId)){
+            return IMOOCJSONResult.errorMsg(null);
+        }
+        CommentLevelCountsVO countsVO = itemService.queryCommentCounts(itemId);
+        return IMOOCJSONResult.ok(countsVO);
+    }
+
+    @ApiOperation(value = "查询商品评论", notes = "查询商品评论", httpMethod = "GET")
+    @GetMapping("/comments")
+    public IMOOCJSONResult comments(
+            @ApiParam(name = "itemId", value = "商品id", required = true)
+            @RequestParam String itemId ,
+            @ApiParam(name = "level", value = "评价等级", required = true)
+            @RequestParam Integer level ,
+            @ApiParam(name = "page", value = "查询下一页的第几页", required = true)
+            @RequestParam Integer page,
+            @ApiParam(name = "pageSize", value = "分页的每一页显示的条数", required = true)
+            @RequestParam Integer pageSize) {
+        //判断itemsId是否为空，为空返回错误
+        if (StringUtils.isBlank(itemId)){
+            return IMOOCJSONResult.errorMsg(null);
+        }
+        if (page==null){
+            page=1;
+        }
+        if (pageSize==null){
+            page=COMMON_PAGE_SIZE;
+        }
+        PagedGridResult pagedGridResult = itemService.queryPagedComments(itemId, level, page, pageSize);
+        return IMOOCJSONResult.ok(pagedGridResult);
     }
 
 }
--- foodie-dev-service/src/main/java/com/imooc/service/impl/ItemServiceImpl.java
+++ foodie-dev-service/src/main/java/com/imooc/service/impl/ItemServiceImpl.java
@@ -1,21 +1,23 @@
 package com.imooc.service.impl;
 
-import com.imooc.mapper.ItemsImgMapper;
-import com.imooc.mapper.ItemsMapper;
-import com.imooc.mapper.ItemsParamMapper;
-import com.imooc.mapper.ItemsSpecMapper;
-import com.imooc.pojo.Items;
-import com.imooc.pojo.ItemsImg;
-import com.imooc.pojo.ItemsParam;
-import com.imooc.pojo.ItemsSpec;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.imooc.enums.CommentLevel;
+import com.imooc.mapper.*;
+import com.imooc.pojo.*;
+import com.imooc.pojo.vo.CommentLevelCountsVO;
+import com.imooc.pojo.vo.ItemCommentVO;
 import com.imooc.service.ItemService;
+import com.imooc.utils.PagedGridResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ItemServiceImpl implements ItemService {
@@ -27,31 +29,38 @@ public class ItemServiceImpl implements ItemService {
     private ItemsSpecMapper itemsSpecMapper;
     @Autowired
     private ItemsParamMapper itemsParamMapper;
+    @Autowired
+    private ItemsCommentsMapper itemsCommentsMapper;
+    @Autowired
+    private ItemsMapperCustom itemsMapperCustom;
 
     @Transactional(propagation = Propagation.SUPPORTS)
     @Override
     public Items queryItemById(String itemId) {
-        Items  result= itemsMapper.selectByPrimaryKey(itemId);
+        Items result = itemsMapper.selectByPrimaryKey(itemId);
         return result;
     }
+
     @Transactional(propagation = Propagation.SUPPORTS)
     @Override
     public List<ItemsImg> queryItemImgList(String itemId) {
-        Example example=new Example(ItemsImg.class);
-        Example.Criteria criteria=example.createCriteria();
-        criteria.andEqualTo("itemId",itemId);
+        Example example = new Example(ItemsImg.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("itemId", itemId);
         List<ItemsImg> result = itemsImgMapper.selectByExample(example);
         return result;
     }
+
     @Transactional(propagation = Propagation.SUPPORTS)
     @Override
     public List<ItemsSpec> queryItemSpecList(String itemId) {
-        Example example=new Example(ItemsSpec.class);
-        Example.Criteria criteria=example.createCriteria();
-        criteria.andEqualTo("itemId",itemId);
+        Example example = new Example(ItemsSpec.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("itemId", itemId);
         List<ItemsSpec> result = itemsSpecMapper.selectByExample(example);
         return result;
     }
+
     @Transactional(propagation = Propagation.SUPPORTS)
     @Override
     public ItemsParam queryItemParam(String itemId) {
@@ -61,5 +70,64 @@ public class ItemServiceImpl implements ItemService {
 
         ItemsParam result = itemsParamMapper.selectOneByExample(itemsParamExp);
         return result;
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public CommentLevelCountsVO queryCommentCounts(String itemId) {
+        //获取评级数量
+        Integer goodCounts = getCommentCounts(itemId, CommentLevel.GOOD.type);
+        Integer normalCounts = getCommentCounts(itemId, CommentLevel.NORMAL.type);
+        Integer badCounts = getCommentCounts(itemId, CommentLevel.BAD.type);
+        Integer totalCounts = goodCounts + normalCounts + badCounts;
+        //设置评级数
+        CommentLevelCountsVO countsVO = new CommentLevelCountsVO();
+        countsVO.setGoodCounts(goodCounts);
+        countsVO.setNormalCounts(normalCounts);
+        countsVO.setBadCounts(badCounts);
+        countsVO.setTotalCounts(totalCounts);
+        return countsVO;
+    }
+
+
+    //获取商品对应评价数量
+    @Transactional(propagation = Propagation.SUPPORTS)
+    Integer getCommentCounts(String itemId, Integer level) {
+        ItemsComments condition = new ItemsComments();
+        condition.setItemId(itemId);
+        if (level != null) {
+            condition.setCommentLevel(level);
+        }
+        condition.setCommentLevel(level);
+        Integer result = itemsCommentsMapper.selectCount(condition);
+        return result;
+    }
+
+    /*
+    根据商品Id查询商品评价
+     */
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public PagedGridResult queryPagedComments(String itemId, Integer level,
+                                              Integer page, Integer pageSize) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("itemId", itemId);
+        map.put("level", level);
+        //mybatis-pagehelper 分页
+        /*** page: 第几页 * pageSize: 每页显示条数 */
+        PageHelper.startPage(page, pageSize);
+        List<ItemCommentVO> list = itemsMapperCustom.queryItemComments(map);
+        PagedGridResult pagedGridResult = setterPagedGrid(list,page);
+        return pagedGridResult;
+    }
+
+    private PagedGridResult setterPagedGrid(List<?> list ,Integer page){
+        PageInfo<?> pageList = new PageInfo<>(list);
+        PagedGridResult grid = new PagedGridResult();
+        grid.setPage(page);
+        grid.setRows(list);
+        grid.setTotal(pageList.getPages());
+        grid.setRecords(pageList.getTotal());
+        return grid;
     }
 }
